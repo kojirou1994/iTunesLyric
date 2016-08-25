83040a396326e9179208d1ad02a7eaf525df9c41
Kojirou <kojirouhtc@gmail.com>
2016-08-26 00:38:25 +0800

    Initial Commit

---
+++ iTunesLyricUITests/iTunesLyricUITests.swift
@@ -0,0 +1,36 @@
+//
+//  iTunesLyricUITests.swift
+//  iTunesLyricUITests
+//
+//  Created by 王宇 on 2016/8/26.
+//  Copyright © 2016年 Putotyra. All rights reserved.
+//
+
+import XCTest
+
+class iTunesLyricUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
